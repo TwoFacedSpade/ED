66ddd7ab1a9270c5b8d87d5c2fbc5b2516a86931
TwoFacedSpade <josegabrielgg@ciencias.unam.mx>
2025-11-04 10:21:56 -0600

    feat  String to binary

--- Proyecto/Proyecto.hs
+++ Proyecto/Proyecto.hs
@@ -4,6 +4,25 @@ import Data.Function
 data Huffa = Vacio | Hoja Char | Nodo Huffa Huffa
 --data Arbol a= Vacio | AB a  (Arbol a) (Arbol a) deriving (Eq, Ord, Show)
 
+
+huffman :: String -> String 
+huffman [] = []
+huffman frase = traductor frase (transformador(recibetexto(frase)))
+
+traductor :: String -> [(Char, String)] -> String
+traductor [] (x:xs) = []
+traductor (y:ys) (x:xs) = tra y (x:xs) ++ traductor ys (x:xs)
+
+tra :: Char -> [(Char, String)] -> String 
+tra x [] = []
+tra x (y:ys) = tr x y ++ tra x ys
+
+tr :: Char -> (Char, String) -> String
+tr x (a,b) = 
+  if x == a 
+  then b 
+  else []
+
 recibetexto :: String -> [(Char, Int)]
 recibetexto [] = []
 recibetexto (x:xs) = sorte( [(x , (1 + aparece x xs ))] ++ recibetexto(eliminar x xs) ) 
@@ -25,8 +44,7 @@ eliminar c (x:xs) =
 sorte :: (Ord b) => [(a,b)] -> [(a,b)]
 sorte = sortBy (flip compare `on` snd)
 
-margo = "01"
-
+margo = "1"
 
 transformador :: [(Char, Int)] -> [(Char, String)]
 transformador [] = []
@@ -35,7 +53,6 @@ transformador (x:xs) = [binario margo (x)]  ++ recorrido ( "0" ++ margo ) xs
 recorrido :: String -> [(Char, Int)] -> [(Char, String)]
 recorrido margo [] = []
 recorrido margo (x:xs) = [binario margo (x)]  ++ recorrido ( "0" ++ margo ) xs  
-
 
 binario :: String -> (Char, Int) -> (Char, String)
 binario margo (a,b) = (a,margo) 
