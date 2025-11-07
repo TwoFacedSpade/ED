0befd476070ef7582b91c5cf43bcc58b7ebc4132
TwoFacedSpade <josegabrielgg@ciencias.unam.mx>
2025-11-06 18:54:55 -0600

    Fix: remove unnecesary functions and improve other functions

--- Proyecto/Aux.hs
+++ Proyecto/Aux.hs
@@ -3,30 +3,10 @@ data Huffa = Vacio | Nodo Huffa Char deriving (Eq, Ord, Show)
 
 {- *****************************************
    ** 
-   **  Hola profe, puede que algunos de los Usos esten mal porque se me olvido probar
-   **  las funciones individualmente y los puse de memoria, lo corregiré en la entrega final :D
+   **   Tabla
    **
    *****************************************
--}
 
-{- *****************************************
-   ** 
-   **  La mayoria de las funciones que me molestan son porque como guardo los valores en tuplas (las tablas)
-   **  necesito una funcion para que recorra la lista de tuplas y otra funcion para que haga las operaciones
-   **  que quiero con esas tuplas el mejor ejemplo seria recorrido que lo unico que hace es añadir un cero a 
-   **  la variable margo y pasar al siguiente elemento de la tupla
-   **
-   *****************************************
--}
-
-
-{- *****************************************
-   ** 
-   **   Tablas
-   **
-   *****************************************
--}
-{-
   Funcion: tablaFrecuencias
   Descripcion: Crea una tabla de frecuencia de las letras en una frase ordenada de manera ascendente  
 -}
@@ -36,8 +16,7 @@ tablaFrecuencias (x:xs) = quicksort( [(x , (1 + cuentaApariciones x xs ))] ++ ta
 
 {-
   Funcion: cuentaApariciones
-  Descripcion: cuenta las veces que aparece una letra en la frase
-  
+  Descripcion: cuenta las veces que aparece una letra en la frase  
 -}
 cuentaApariciones :: Char -> String -> Int
 cuentaApariciones c [] = 0
@@ -73,13 +52,13 @@ quicksort ((a,b):xs) =
 
 {- *****************************************
    ** 
-   **   fin Tablas
+   **   fin Tabla
    **
    *****************************************
--}
 
+*****************************************************************************
 
-{- *****************************************
+   *****************************************
    ** 
    **   Arbol
    **
@@ -93,25 +72,17 @@ quicksort ((a,b):xs) =
 -}
 arbol :: [(Char, Int )] -> Huffa
 arbol [] = Vacio
-arbol (x:xs) =  Nodo (arbol xs) (nodito x)
-
-{-
-  Funcion: nodito
-  Descripcion: devuelve el char de la tupla
-  Uso: nodito ('a',1) = a
--}
-nodito :: (Char, Int) -> Char
-nodito (a,b) = a
+arbol ((a,b):xs) =  Nodo (arbol xs) (a)
 
 {- *****************************************
    ** 
    **   fin arbol 
    **
    *****************************************
--}
+  
+*****************************************************************************
 
-
-{- *****************************************
+   *****************************************
    ** 
    **   binarios
    **
@@ -123,30 +94,20 @@ nodito (a,b) = a
   Uso: traductor "hambre" [('h',"1"),('a',"01"),('m',"001"),('b',"0001"),('r',"00001"),('e',"000001") ]   = "101001000100001000001"
 -}
 traductor :: String -> [(Char, String)] -> String
-traductor [] (x:xs) = []
-traductor (y:ys) (x:xs) = tra y (x:xs) ++ traductor ys (x:xs)
+traductor [] arbol = []
+traductor (y:ys) arbol =  buscaTabla y arbol ++ traductor ys arbol 
 
 {-
-  Funcion: tra
-  Descripcion: evalua una letra de la frase con su tabla de binarios
-  Uso: tra "h" [('h',"1"),('a',"01"),('m',"001"),('b',"0001"),('r',"00001"),('e',"000001") ]   = "h"
+  Funcion: buscaTabla
+  Descripcion: Busca un elemento de la cadena original en el arbol binario e intercambia su valor
+  Uso: buscaTabla "a" [('h',"1"),('a',"01"),('m',"001"),('b',"0001"),('r',"00001"),('e',"000001") ] = "01"
 -}
-tra :: Char -> [(Char, String)] -> String 
-tra x [] = []
-tra x (y:ys) = tr x y ++ tra x ys
-
-
-{-
-  Funcion: tr
-  Descripcion: evalua una letra de la frase con una tupla de su tabla de binarios
-  Uso: tr "h" ('h',"1")   = "h"
-       tr "a" ('h',"1")   = []
--}
-tr :: Char -> (Char, String) -> String
-tr x (a,b) = 
-  if x == a 
+buscaTabla :: Char -> [(Char, String)] -> String
+buscaTabla c [] = []
+buscaTabla c ((a,b):xs) = 
+  if c == a 
   then b 
-  else []
+  else buscaTabla c xs
 
 -- variable para la cadena binaria de cada letra
 margo = "1"
@@ -158,27 +119,16 @@ margo = "1"
 -}
 transformador :: [(Char, Int)] -> [(Char, String)]
 transformador [] = []
-transformador (x:xs) = [binario margo (x)]  ++ recorrido ( "0" ++ margo ) xs  
+transformador (x:xs) = transformadorAux margo  (x:xs)
 
 {-
-  Funcion: recorrido 
-  Descripcion: recorre la lista añadiendo 0 a la variable margo para cambiar el valor del binario
-  Osea que cambia el valor del binario y va al siguiente valor de la lista
-  realmente no hace gran cosa, ¿podria eliminarlo?
+  Funcion: transformadorAux 
+  Descripcion: cambia el entero de un elemento de la tabla de frecuencias por su valor binario
   Uso: recorrido "1" [('a',9), ('b',2), ('c',1)] -> recorrido "01" [('b',2), ('c',1)]
 -}
-recorrido :: String -> [(Char, Int)] -> [(Char, String)]
-recorrido margo [] = []
-recorrido margo (x:xs) = [binario margo (x)]  ++ recorrido ( "0" ++ margo ) xs  
-
-{-
-  Funcion: binario
-  Descripcion: Cambia el entero del elemento por una cadena que represente su valor binario
-  Uso: binario "1" ('a',9) = ('a',"1")
--}
-binario :: String -> (Char, Int) -> (Char, String)
-binario margo (a,b) = (a,margo) 
-
+transformadorAux :: String -> [(Char, Int)] -> [(Char, String)]
+transformadorAux margo [] = []
+transformadorAux margo ((a,b):xs) = [(a,margo)]  ++ transformadorAux ( "0" ++ margo ) xs  
 
 {- *****************************************
    ** 
@@ -193,64 +143,30 @@ binario margo (a,b) = (a,margo)
    **  decodificadores
    **
    *****************************************
--}
-{-
+
   Funcion: decodificador
   Descripcion: Decodifica un binario de acuerdo con su arbol Huffa 
   Uso: decodificador ("101001",Nodo (Nodo (Nodo Vacio 'c') 'b') 'a') = "abc"
 -}
 decodificador :: (String, Huffa) -> String
 decodificador ([] , huff) = []
-decodificador ( (x:xs) , huff ) = check (x:xs) huff  ++ decodificador ( (ceros (x:xs) ) , huff)
+decodificador ( frase , huff ) = decodeAux frase huff huff 
 
 
 {-
-  Funcion: check
-  Descripcion: evalua una letra del binario de acuerdo a su arbol Huffa
-  Uso: check 101001 huffa = "a"
-  
-  suponiendo que el arbol se ve asi: 
-  huffa = Nodo (Nodo (Nodo Vacio 'c' ) 'b') 'a'
+  Funcion: decodeAux
+  Descripcion: Recorre el arbol binario y devuelve los caracteres segun el binario
+  Uso: decodificador ("101001",Nodo (Nodo (Nodo Vacio 'c') 'b') 'a') = "abc"  
 -}
-check :: String -> Huffa -> String
-check (x:xs) (Nodo ti a) = 
-  if x == '1'
-  then [a]
-  else check xs ti 
+decodeAux :: String -> Huffa -> Huffa -> String
+decodeAux [] (Nodo iz c) original = []
+decodeAux ('0':res) (Nodo iz _) original = decodeAux res iz original
+decodeAux ('1':res) (Nodo _ c) original = [c] ++ decodeAux res original original
 
-
-{-
-  Funcion: ceros
-  Descripcion: Elimina los ceros innecesarios de una letra que ya se evaluo por check
-  Uso: ceros 001000010001 = 000010001
--}
-ceros :: String -> String
-ceros [] = []
-ceros (x:xs) = 
-  if x == '1' 
-  then xs
-  else ceros xs
 
 {- *****************************************
    ** 
    **  fin  decodificadores 
    **
    *****************************************
--}
-
-{-
-   +--------------+
-   |.------------.|
-   ||  Gracias   ||
-   ||  Profe     ||
-   ||  Irvin     ||
-   ||  :D        ||
-   |+------------+|
-   +-..--------..-+
-   .--------------.
-  / /============\ \
- / /==============\ \
-/____________________\
-\____________________/
-
 -}
